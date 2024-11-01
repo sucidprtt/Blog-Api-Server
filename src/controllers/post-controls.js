350e98182025d8a78ec0b5b202ea46fa9941330c
sucidprtt <pratiwisucidwii@gmail.com>
2024-11-01 15:53:09 +0700

    feat: Update and Delete Blog Post Endpoint

--- src/controllers/post-controls.js
+++ src/controllers/post-controls.js
@@ -1,4 +1,5 @@
 import Post from "../models/post-models.js";
+import { uploadRemover } from "../utils/uploadRemover.js";
 
 export const createPost = async (req, res) => {
     const { title, body } = req.body;
@@ -12,9 +13,10 @@ export const createPost = async (req, res) => {
             message: "Post Created",
             data: savedPost
         })
+
     } catch (error){
         console.error(error)
-        return res.status(500).json({ message: "Unable to create post"})
+        return res.status(500).json({ message: "Unable to Create Post"})
     }
 }
 
@@ -23,13 +25,13 @@ export const getPosts = async (req, res) => {
         const posts = await Post.find()
 
         return res.status(200).json({
-            message: "Posts found",
+            message: "Posts Found",
             data: posts
         })
 
     } catch (error){
         console.error(error)
-        return res.status(500).json({ message: "Failed to fetch posts" })
+        return res.status(500).json({ message: "Failed to Fetch Posts" })
     }
 }
 
@@ -40,14 +42,71 @@ export const getPostById = async (req, res) => {
         const post = await Post.findById(postId)
 
         if (!post){
-            return res.status(404).json({ message: "Post not found" })
+            return res.status(404).json({ message: "Post Not Found" })
         }
         return res.status(200).json({
-            message: "Post found",
+            message: "Post Found",
             data: post
         })
+
     }catch (error){
         console.error(error)
-        return res.status(500).json({ message: "Error retrieving post" })
+        return res.status(500).json({ message: "Error Retrieving Post" })
+    }
+}
+
+export const updatePost = async (req, res) => {
+    const postId = req.params.id
+    const { title, body } = req.body
+    const newImage = req.file ? req.file.filename : null
+
+    try{
+        const post = await Post.findById(postId)
+
+        if(!post){
+            return res.status(404).json({ message: "Post Not Found"})
+        }
+
+        if (newImage) {
+            uploadRemover(post.image)
+        }
+
+        post.title = title
+        post.body = body
+        post.image = newImage ? newImage : post.image
+
+        const updatePost = await post.save()
+
+        return res.status(200).json({
+            message: "Post Updated",
+            data: updatePost
+        })
+
+    } catch (error){
+        console.error(error)
+        return res.status(500).json({ message: "Error Updating Post" })
+    }
+}
+
+export const deletePost = async (req, res) => {
+    const postId = req.params.id;
+
+    try {
+        const post = await Post.findByIdAndDelete(postId)
+
+        if (!post) {
+            return res.status(404).json({ message: "Post Not Found" })
+        }
+
+        uploadRemover(post.image)
+
+        return res.status(200).json({
+            message: "Post Deelted",
+            data: post
+        })
+
+    } catch (error){
+        console.error(error)
+        return res.status(500).json({ message: "Error Deleting Post" })
     }
 }
